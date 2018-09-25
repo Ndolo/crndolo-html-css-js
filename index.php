7ba96cb7cd444cb0e2bdc9b2b4710e71f31f8027
crndolo <ndoloraphael@gmail.com>
2018-09-25 21:27:35 +0300

    This is the first commit of my personal website design. The site is made purely using HTML, CSS, Javascript and a bit of PHP

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+	<head>
+			<!-- Site Title -->
+			<title> crndolo </title>
+			<!-- Custom Style -->
+			<link href="css/styles.css" rel="stylesheet" type="text/css">
+			<!-- Bootrstrap CSS -->
+			<link href="css/bootstrap.min.css" rel="stylesheet" type="text/css">
+	</head>
+	<body>
+
+		<div class="container-fluid main-body">
+			
+			<div class="row">
+				<!-- Three column to display image and menu-->
+				<div class="col-md-3 header-section">
+
+					<!-- Twelve column display image and personal Data-->
+					<div class="row">
+						<div class="col-md-12">
+							<h1 class="site-title">
+								Raphael Ndolo
+							</h1>
+							<p class="site-description">
+								Full stack Web Developer
+							</p>
+						</div>
+					</div>
+				</div>
+
+				<!-- Eight column to display site body-->
+				<div class="col-md-9 main-body-section">
+					Eight Column
+				</div>
+			</div>
+		</div>
+
+		<!-- Scripts -->
+		<script src="js/jquery-3.3.1.min.js"> </script>          
+        <script src="js/bootstrap.min.js"> </script>
+	</body>
+</html>
